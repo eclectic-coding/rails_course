a1836ab131a4210624cfe2c2887d5b9afad5b4d1
eclecticcoding <chuck@eclecticsaddlebag.com>
2021-07-27 08:21:53 -0400

    Add user associations to courses

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,7 +1,7 @@
 class Course < ApplicationRecord
+  belongs_to :user
   has_rich_text :description
 
   validates :title, presence: true
   validates :description, presence: true, length: { minimum: 5 }
-
 end
